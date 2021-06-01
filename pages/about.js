d63e6f303e9c7380343a574b20995155a52d0a8d
208417021 <208417021@gms.tku.edu.tw>
2021-06-01 19:06:42 +0800

    20210601

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+            <h1>about</h1>
+            <p>This is an app</p>
+            <p>Version: 0.0.0.0.8.7</p>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AddEventsPage() {
+    return <div>add event page</div>;
+}
